7abc63c859c31b8d1b8064323738d92914f25e80
brandonstoiber <brandonstoiber@brandons-air>
2017-02-16 20:48:32 -0600

    Created course class and added abstract method.

---
+++ src/lab2/Course.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author brandonstoiber
+ */
+public interface Course {
+    //abstract getter methods
+    public abstract String getCourseNumber();
+    public abstract String getCourseName();
+    public abstract double getNumberOfCredits();
+    
+    //abstract setter methods
+    public abstract void setCourseNumber(String courseNumber);
+    public abstract void setCourseName(String courseName);
+    public abstract void setNumberOfCredits(double numberOfCredits);
+    
+}
